e12d7e585270e5db9fcfc06eb1860ac9c5c1cac1
Alex Sander Resende de Deus <prof.alexresende@gmail.com>
2021-10-04 11:03:43 -0300

    Exemplo de uso do radiobutton

---
+++ radioButton/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace radioButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChecar_Click(object sender, RoutedEventArgs e)
+        {
+            if (rdbMasculino.IsChecked == true)
+            {
+                MessageBox.Show("Selecionado sexo masculino", "RadioButton",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Selecionado sexo feminino", "RadioButton",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
